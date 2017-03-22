46a53cb02f9016eaa75cb2e0cef804c4b5704540
Anargyros Tsadimas <tsadimas@gmail.com>
2017-03-22 20:43:26 +0200

    Added redirect url option

--- getposts/settings.py
+++ getposts/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = '/static/'
 #registration
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
+LOGIN_REDIRECT_URL = '/'
